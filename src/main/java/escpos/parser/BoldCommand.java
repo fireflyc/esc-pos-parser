dae7ba66fcbe35ce0079bf34cfb908521866fe29
fireflyc <fireflyc@126.com>
2014-04-25 21:15:05 +0800

    遗漏的文件

---
+++ src/main/java/escpos/parser/BoldCommand.java
@@ -0,0 +1,25 @@
+package escpos.parser;
+
+import org.dom4j.Element;
+
+/**
+ * User: xingsen
+ * Date: 14-4-25
+ * Time: 下午2:47
+ */
+public class BoldCommand extends Command {
+    @Override
+    public String tag() {
+        return "b";
+    }
+
+    @Override
+    protected byte[] end(Element self) {
+        return new byte[]{0x1B, 0x45, 0};
+    }
+
+    @Override
+    protected byte[] start(Element self) {
+        return new byte[]{0x1B, 0x45, 1};
+    }
+}
